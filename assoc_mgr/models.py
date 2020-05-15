ab30a03a8d74c9467eef3d422f6456b754dc7a2c
Jeff Walton <jwalton@paulsmiths.edu>
2020-05-14 21:16:17 -0400

    database

--- assoc_mgr/models.py
+++ assoc_mgr/models.py
@@ -16,8 +16,8 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True, index=True, nullable=False)
-    users = db.relationship('User', backref='role')
-    assocs = db.relationship('Association', backref='role')
+    users = db.relationship('User', backref='role', lazy='dynamic')
+    assocs = db.relationship('Association', backref='role', lazy='dynamic')
 
     def __repr__(self):
         return f"<Role {self.name}>" 
