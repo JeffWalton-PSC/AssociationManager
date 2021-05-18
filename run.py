f9ef8e283912d310f22258a80f9eaae34610ec28
JeffWalton-PSC <jwalton@paulsmiths.edu>
2021-05-18 15:40:44 -0400

    updates for new server

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ app = create_app()
 
 # Main
 if __name__ == "__main__":
-    # app.run(host="10.4.204.100")
-    app.run(host="127.0.0.1")
+    app.run(host="10.4.204.101")
+    # app.run(host="127.0.0.1")
     # Set Debugger to True to use Flask Debugger. Reccomended to be used only when server is offline.
     # If debug is left True while server is in production, then there is potential to expose data.
